03132439adca54d4881029220d82d4189b8626d9
Rose Wachuka <67165120+ro61zzy@users.noreply.github.com>
2023-02-14 19:39:08 +0300

    client side MUI

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import DrawerAppBar from "./Components/Appbar/ElevateAppbar";
+
+function App() {
+  return (
+    <div className="App">
+      <DrawerAppBar />
+    </div>
+  );
+}
+
+export default App;
