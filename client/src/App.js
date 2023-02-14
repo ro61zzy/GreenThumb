6e408606c779b71d1b913efba6c80caf5a1e512c
Rose Wachuka <67165120+ro61zzy@users.noreply.github.com>
2023-02-14 20:19:30 +0300

    cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import DrawerAppBar from "./Components/Appbar/ElevateAppbar";
 
@@ -6,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <DrawerAppBar />
+      
     </div>
   );
 }
---
+++ client/src/Components/Card/BasicCard.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import { Box } from '@mui/system';
+
+export default function MediaCard() {
+  return (
+    <Box sx={{display:"flex", gap:"10px"}}>
+    <Card sx={{ maxWidth: 330 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          PlantName
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+        </Typography>
+      </CardContent>
+      <CardActions sx={{color:"green", justifyContent:"center", gap:"20px"}}>
+        <FavoriteBorderIcon sx={{fontSize:"42px"}} />
+        <WhatsAppIcon sx={{fontSize:"42px"}} />
+      </CardActions>
+    </Card>
+    <Card sx={{ maxWidth: 330 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          PlantName
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+        </Typography>
+      </CardContent>
+      <CardActions sx={{color:"green", justifyContent:"center", gap:"20px"}}>
+        <FavoriteBorderIcon sx={{fontSize:"42px"}} />
+        <WhatsAppIcon sx={{fontSize:"42px"}} />
+      </CardActions>
+    </Card>
+    </Box>
+  );
+}
