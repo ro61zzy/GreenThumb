58a068e466466cabb23d65d9bc4d64c896f94717
Rose Wachuka <67165120+ro61zzy@users.noreply.github.com>
2023-02-20 16:29:24 +0300

    fix padding

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import LogIn from "./Components/SignIn/LogIn";
 import SignUp from "./Components/SignUp/SignUp";
 import DrawerAppBar from "./Components/Appbar/ElevateAppbar";
 import Favourites from "./Components/Favorites/Favorites";
-import Footer from "./Components/Footer/Footer";
+//import Footer from "./Components/Footer/Footer";
 import HeroSection from "./Components/Hero/HeroSection";
 import MediaCard from "./Components/Card/BasicCard";
 import Description from "./Components/Description/Description";
@@ -22,7 +22,7 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/description" element={<Description />} />
         </Routes>
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </BrowserRouter>
   );
