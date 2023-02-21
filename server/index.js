f78a057aaa103cc105303ea58300f19d68af9b39
Rose Wachuka <67165120+ro61zzy@users.noreply.github.com>
2023-02-21 13:08:30 +0300

    set up server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
- 
+const express = require("express");
+const app = express();
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Green thumb')
+  })
+
+
+
+
+
+app.listen(4000, () => {
+  console.log(`Node API is running on port 4000`)
+});
